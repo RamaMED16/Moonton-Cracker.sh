2c4845ef84433e665eb1d3c39b2c61b8a737606c
RamaMED16 <53370134+RamaMED16@users.noreply.github.com>
2020-06-27 03:18:05 +0700

    Akun.Moonton.sh
    Tutorial makenya
    Di Channel YT :Rama ahmed

---
+++ Akun-Moonton.sh
@@ -0,0 +1,458 @@
+#Gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+pkg install toilet -y
+bash jr.sh
+echo
+echo  $green
+echo "Loading.............";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR GOBLOK.........................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................';
+sleep 0.1;
+clear
+echo  $red
+echo 'SABAR GOBLOK............................................';
+sleep 0.4;
+echo  $green
+echo "SABAR GOBLOK..................................";
+sleep 0.1;
+clear
+echo  $green
+echo  'Harap Sabar...............';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar...........................';
+sleep 0.1;
+echo  $green
+echo "Loading.........................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar....................................................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading GOBLOK................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK............................................';
+sleep 0.1;
+echo  $green
+echo "Loading.....................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar GOBLOK.............................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading........................................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar GOBLOK........................';
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'Harap Sabar................';
+sleep 0.1;
+clear
+echo  $green
+echo 'Loading.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar ANJG...................................................';
+sleep 0.4;
+echo  $green
+echo "Loading....................................";
+sleep 0.1;
+clear
+echo  $green
+echo "Loading.............................................";
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR KONTOL................';
+sleep 0.1;
+clear
+echo  $green
+echo 'SABAR NGENTOD.................................';
+sleep 0.1;
+clear
+echo  $red
+echo 'Harap Sabar BANGSD...................................................';
+sleep 0.4;
+echo  $green
+echo "NAH BERES ANJG....................................";
+sleep 0.1;
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future "    SOLO PLAYER" | lolcat
+echo
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $yellow
+figlet -f future "Github.com/RamaMED16"
+echo
+echo $red"##########################$green" Rama Ahmed R "$red######################"
+echo
+echo $indigo"	~"$cyan"{"$white"A"$cyan"}"$green"."$yellow"Akun Moonton Cracker"$cyan
+echo $indigo"        ~"$cyan"{"$white"B"$cyan"}"$green"."$yellow"Coming Soon 500Subs"$cyan
+echo $indigo"	~"$cyan"{"$white"C"$cyan"}"$green"."$yellow"Coming Soon 700subs"$cyan
+echo
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $indigo"  +"$cyan"{"$white"IG"$cyan"}"$purple"."$green"@Ramaahmd_"$indigo"      +"$cyan"{"$white"I"$cyan"}"$green"Info"$indigo"      +"$cyan"{"$white"Q"$cyan"}"$green"Quit"$cyan
+echo $cyan"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo $indigo"=============================================================="
+echo
+echo $white "╭─"$yellow"Mr.XaHmd022"$cyan" ~MED"$white
+read -p " ╰─$ "  apaan
+echo
+if [ $apaan = "A" ] || [ $apaan = "a" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "Akun Moonton Cracker" | lolcat
+echo
+echo $red "Note:Akun Random Banyak yang di Banned"
+echo
+echo $yellow "Random Akun"
+echo $green "feronanda02@gmail.com:Nanda12345" 
+sleep 1
+echo $green "bugagay16@gmail.com:Sarhan13" 
+sleep 1
+echo $green "monol32@mail.ru:WhlsW7dm"
+sleep 1
+echo $green "qwertyjackblack@gmail.com:Hbxfhl310796 "
+sleep 1
+echo $green "davidbicer@gmx.net:RAje4791" 
+sleep 1
+echo $green "genesis.situmorang@gmail.com:new436178MPd"
+sleep 1
+echo $green "arjeluka@gmail.com:J5aar4j9 "
+sleep 1
+echo $green "railexgamer1@gmail.com:Jd234567 "
+sleep 1
+echo $green "coco.tkatz@gmail.com:Magic224639 "
+sleep 1
+echo $green "alldienick@gmail.com:QWEasd123 "
+sleep 1
+echo $green "glays@live.ru:Leon180597 "
+sleep 1
+echo $green "fajar.knight@gmail.com:Setan123 "
+sleep 1
+echo $green "paul_sabater@hotmail.com:Crystalcore15 "
+sleep 1
+echo $green "jomakmakovich@gmail.com:Maksim1450 "
+sleep 1
+echo $green "semaust@yandex.ru:SINIMAsema88 "
+sleep 1
+echo $green "bigtokes96@gmail.com:Jeepsrt8 "
+sleep 1
+echo $green "iris.alarconp@gmail.com:123lolispolisA "
+sleep 1
+echo $green "franklincheung09@gmail.com:Faker123 "
+sleep 1
+echo $green "ninjablow8@gmail.com:Jayden02 "
+sleep 1
+echo $green "rajadaudz@gmail.com:Peaceboy123 "
+sleep 1
+echo $green "mateokershner@gmail.com:Yoshimitsu23 "
+sleep 1
+echo $green "andrewclark12@gmail.com:Raf5Uve3 "
+sleep 1
+echo $green "norman.sabi@gmail.com:Ratamahatta9 "
+sleep 1
+echo $green "bastek001@gmail.com:Klose123 "
+sleep 1
+echo $green "limotion.misteri@gmail.com:RealityVnero123 "
+sleep 1
+echo $green "marko9711@gmail.com:Drazpowa121 "
+sleep 1
+echo $green "darkjainra@gmail.com:Supernova52 "
+sleep 1
+echo $green "wilburpatrick@gmail.com:Ctl5xnxn "
+sleep 1
+echo $green "ultrav33@gmail.com:Mongoman666 "
+sleep 1
+echo $green "mr.dryn4er@gmail.com:Grasshopper01 "
+sleep 1
+echo $green "lucutzu@yahoo.com:Spiderman2001 "
+sleep 1
+echo $green "conanvilla@hotmail.com:Blacky123"
+sleep 1
+echo $green "ivan02061994@mail.ru:LOmtsP9G"
+sleep 1
+echo $green "angelaxlefebvre@gmail.com:Forever1 "
+sleep 1
+echo $green "bearvoorhis2013@gmail.com:august30TH"
+sleep 1
+echo $green "yshammala@gmail.com:Joker123 "
+sleep 1
+echo $green "michael.siren@yahoo.co.id:Lokialfa23 "
+sleep 1
+echo $green "salmanslah95@gmail.com:Salm12345 "
+sleep 1
+echo $green "ar4ibaltweek@gmail.com:yjdsqGfhjkm1 "
+sleep 1
+echo $green "nizamutdinov-2002@mail.ru:Pfdthitybt2 "
+sleep 1
+echo $green "stasic04@mail.ru:Bonya2011 "
+sleep 1
+echo $green "razer0073@gmail.com:Vtxtysq1 "
+sleep 1
+echo $green "matiaspiasentini@hotmail.com.ar:Naruto25 "
+sleep 1
+echo $green "evgen.martynov@gmail.com:123Jekki "
+sleep 1
+echo $green "millerquirona@gmail.com:Miller14 "
+sleep 1
+echo $green "vladuteu@gmail.com:Andrei98 "
+sleep 1
+echo $green "nazgul@inbox.ru:D8bdcb63 "
+sleep 1
+echo $green "arnaud.lente@gmail.com:Azerty80"
+sleep 1
+echo $green "felix_1788@mail.ru:M1a2m3a4 "
+sleep 1
+echo $green "rama_3sakti@yahoo.com:Rama4321 "
+sleep 1
+echo $green "sbmendel@gmail.com:Sarah4554 "
+sleep 1
+echo $green "soulprophet_69@yahoo.com:Zapopan7 "
+sleep 1
+echo $green "askerovrufat123@gmail.com:Rufat123 "
+sleep 1
+echo $green "cristiandramba365@gmail.com:Leopard12 "
+sleep 1
+echo $green "anonymous.master.one@gmail.com:Destiny1 "
+sleep 1
+echo $green "norman.sabi@gmail.com:Ratamahatta9"
+sleep 1
+echo $green "roblewis65@gmail.com:Loser420 "
+sleep 1
+echo $green "timothy.rp@gmail.com:Timmy1504 "
+sleep 1
+echo $green "rizq134@gmail.com:Bandung210587 "
+sleep 1
+echo $green "razeromeo@gmail.com:Gibran98 "
+sleep 1
+echo $green "kiril.gayduk0v@gmail.com:Aa42312413 "
+sleep 1
+echo $green "kerigon5@gmail.com:Djlis007 "
+sleep 1
+echo $green "03popop78@gmail.com:Lastsupper21"
+sleep 1
+echo $green "ernedraugas@gmail.com:Ernis007		 "
+sleep 1
+echo $green "emirunlusoy2002@gmail.com:Emir3194353 "
+sleep 1
+echo $green "brianhutsongp@gmail.com:Tinakay1	"
+sleep 1
+echo $green "dopaspos@gmail.com:7830956Susoms	" 
+sleep 1
+echo $green "mgesual2@gmail.com:Weebus5188		 "
+sleep 1
+echo $green "david.gyenei@gmail.com:Duo080627"	 
+sleep 1
+echo $green "m4d3bym3@gmail.com:Alex1995	 "
+sleep 1
+echo $green "h.vallim@gmail.com:Jhun1984	 "
+sleep 1
+echo $green "taylorjoverton@gmail.com:Barney123	 "
+sleep 1
+echo $green "jgs5093@gmail.com:Skagen3845		 "
+sleep 2
+echo $green "encoregame1337@gmail.com:Bizzar44"	 
+sleep 1
+echo $green "dgibby11@gmail.com:Parker07		 "
+sleep 1
+echo $green "hucincraft@gmail.com:Coco2000	 "
+sleep 1
+echo $green "andrei2004@gmail.com:Passw0rd	 "
+sleep 1
+echo $green "danny.kimm44@gmail.com:Lollig17	 "
+sleep 1
+echo $green "danielluiz94@gmail.com:Paraquedas1" 
+sleep 1
+echo $green "matadoresxdgmailcomMxd468200	"
+sleep 1
+echo $green "blackzodiac019@gmail.com:Pygmalio019 "
+sleep 1
+echo $green "sugiharto.peter@gmail.com:Iloveme1"
+sleep 1
+echo $green "americanopaio@gmail.com:Nignig69	"
+sleep 1
+echo $green "normangameng@gmail.com:041679Madz "
+sleep 1
+echo $green "reci.albi@gmail.com:Twingo97		 "
+sleep 1
+echo $green "chaz.reiter@gmail.com:Kahlua04		 "
+sleep 1
+echo $green "maxplayers57@gmail.com:Robalo11		 "
+sleep 1
+echo $green "adelardick@gmail.com:Valera106749174 "
+sleep 1
+echo $green "ksdmdks@gmail.com:Abc64296429		 "
+sleep 1
+echo $green "vadekskij@gmail.com:Dartray213		 "
+sleep 1
+echo $green "korzyuk80@gmail.com:Kirill228		 "
+sleep 1
+echo $green "kakoccc@gmail.com:Kakoc29041987		 "
+sleep 1
+echo $green "saikat.khan.net@gmail.com:Ss162867	 "
+sleep 1
+echo $green "tkrahling@gmail.com:Kameron16		 "
+sleep 1
+echo $green "golikov.n37@gmail.com:Desirash37	 "
+sleep 1
+echo $green "metzus@gmail.com:At258dxi		"
+sleep 1
+echo $green "vishnu6456@gmail.com:Awsome54321	 "
+sleep 1
+echo $green "ibnuakil93@gmail.com:Ibnuakil93	"	 
+sleep 1
+echo $green "robokop435@gmail.com:Action1992"		 
+sleep 1
+echo $green "rinalrio44@gmail.com:Rinaldi26		 "
+sleep 1
+echo $green "muslehahmad766@gmail.com:L4yyinah	"
+sleep 1
+echo $green "jugandops4@gmail.com:Diego826		 "
+sleep 1
+echo $green "mateus.tjs3@gmail.com:Mateus2004	 "
+sleep 1
+echo $green "faniakhyar87@gmail.com:Ledok123gedi	 "
+sleep 1
+echo $green "ranggasa310@gmail.com:Rangga2018	 "
+sleep 1
+echo $green "langsadafitra@gmail.com:Dk7520di	 "
+sleep 1
+echo $green "phamminhthanh1907@gmail.com:19071986Pvm	 "
+sleep 1
+echo $green "nicologalinato35@gmail.com:Qwerty12345 "
+sleep 1
+echo $green "ilhansandagang7@gmail.com:Ilhan007	 "
+sleep 1
+echo $green "udhasoul@gmail.com:31031990Udha		 "
+sleep 1
+echo $green "unaymirta@gmail.com:Uncle347		 "
+sleep 1
+echo $green "gen.24w@gmail.com:Ziziqiqi7		 "
+sleep 1
+echo $green "seliirawan12@gmail.com:Gapero1996"		 
+sleep 1
+echo $green "candyz196hacker@gmail.com:Mayor123		 "
+sleep 1
+echo $green "kusupvadil@gmail.com:Pendekarmuda123	 "
+sleep 1
+echo $green "hardymargono@gmail.com:17Januari2017"	 
+sleep 1
+echo $green "paracuan90@gmail.com:Jack135791	"	 
+sleep 1
+echo $green "gabriel121vitor@gmail.com:Gabriel121	 "
+sleep 1
+echo $green "pramudya_ti08@yahoo.com:Gauntlet123"	 
+sleep 1
+echo $green "matheusmeirelles@hotmail.com.br:741Mmatheus"	 
+sleep 1
+echo $green "javelinthrow1@yahoo.com:Ivanov2116	"
+sleep 1
+echo $red "Done Next V2 Jika 500subs"
+echo $green "╭─"$blue"Mr.XaHmd022"$cyan" ~ICA"$white
+read -p " ╰─$ "  target
+echo
+echo $red " Arigatou Gozaimasu "
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] : " back
+fi
+
+if [ $apaan = "I" ] || [ $apaan = "i" ]
+then
+echo
+echo
+echo
+clear
+echo
+echo
+echo
+echo
+toilet -f future "   Informasi Tools" | lolcat
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $green"Author  : Mr.XaHmd022 		"
+echo $purple"INSTAGRAM   : @ahmed_78988"
+echo $white"Blog    : http://blogspot-Deface.com"
+echo
+echo $yellow"                          THANKS TO :"
+echo
+echo $cyan"[+] EMAAKK  "
+echo $cyan"[+] BAPAAKK "
+echo $cyan"[+] ALLAH SWT "
+echo $red"##########################$green" WELCOME "$red###########################"
+echo $indigo"＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿＿"
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "T" ] || [ $apaan = "t" ]
+then
+cat tutorial.txt
+echo
+echo $cyan"["$yellow"B"$cyan"]"$white"back "$cyan"["$yellow"Q"$cyan"]"$white"Quit"
+read -p "[B/Q] " back
+fi
+
+if [ $apaan = "Q" ] || [ $apaan = "q" ]
+then
+echo " Terimakasih Sudah Menggunakan Tools Kami.. Follow Ig saya @Ramaahmd_ "
+exit
+clear
+fi
+
+
+if [ $back = "B" ] || [ $back = "b" ]
+then
+sh Akun-Moonton.sh
+fi
+
+if [ $back = "Q" ] || [ $back = "q" ]
+then
+clear
+fi
